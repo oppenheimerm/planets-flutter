006f77df2e0790ef008854ed9efd1690a20063a8
Michiel Oppenheimer <michieloppenheimer@gmail.com>
2018-12-03 16:29:17 +0000

    planet card added

---
+++ flutter_planets/lib/common_widgets/home_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planets/common_widgets/planet_row.dart';
+
+//  Represents a body element for our home page
+class HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new PlanetRow();
+  }
+}
