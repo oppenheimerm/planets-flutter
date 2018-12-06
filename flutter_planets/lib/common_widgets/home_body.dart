78fda9065c40af5c3b96e377795e400cc6ab75fc
Michiel Oppenheimer <michieloppenheimer@gmail.com>
2018-12-06 16:13:13 +0000

    routing-nav

--- flutter_planets/lib/common_widgets/home_body.dart
+++ flutter_planets/lib/common_widgets/home_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_planets/common_widgets/planet_row.dart';
+import 'package:flutter_planets/models/planet_model.dart';
 
 //  Represents a body element for our home page
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new PlanetRow();
+    return new Column(
+      children: <Widget>[
+        new PlanetRow(planets[0]),
+        new PlanetRow(planets[1]),
+        new PlanetRow(planets[2]),
+      ],);
   }
 }
